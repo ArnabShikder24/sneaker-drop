cd8ed677e7552957c9e5c4b360e53b91c52a2190
ArnabShikder24 <arnabshikder2001@gmail.com>
2026-08-20 16:25:32 +0600

    refactor: remove unused useState hook from ReserveButton component

--- src/features/drops/components/ReserveButton.tsx
+++ src/features/drops/components/ReserveButton.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Button } from '../../../components/Button';
 import { useReserve } from '../hooks/useReserve';
 import { useCountdown } from '../hooks/useCountdown';
